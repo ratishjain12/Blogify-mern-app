05c3631cea9f89676ff4d7974a4add1d3f0c8722
ratishjain <ratishjain6@gmail.com>
2023-07-18 14:53:38 +0530

    origin update

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = "zfsfgasfnjrjgwrhoghasckasnfspkovrwvk0vkoaihnas";
 
 app.use(
-  cors({ credentials: true, origin: "https://blogify-mern-app.vercel.app/" })
+  cors({ credentials: true, origin: "https://blogify-mern-app.vercel.app" })
 );
 app.use(express.json());
 app.use(cookieParser());
